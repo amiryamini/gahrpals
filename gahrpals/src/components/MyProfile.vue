5db44c2122eb7f5cf6d854ccb8fcaec5335083e6
aprilLima <paperlima@gmail.com>
2018-05-03 12:49:37 -0700

    working on My Profile page; top info for user is templated so far

---
+++ gahrpals/src/components/MyProfile.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+
+
+<!--scripts for stuff-->
+<script>
+
+</script>
+
+
+
+
+<!-- Added "scoped" attribute to limit CSS to this component only --><!-- Added "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
