f41af2aee7b3dedfa787f060cc5bb826f0ffaab0
amiryamini <amir.yamini.416@my.csun.edu>
2018-05-07 03:04:14 -0700

    Start Login Auth
    I have started the login auth. added new dependencies to package.json for login. also changed name of server.js to app.js (not really important but I did it). added a few new folders that I will be using shortly. Also installed nodemon to have continue server running and updating.

--- gahrpals/app.js
+++ gahrpals/app.js
@@ -1,5 +1,53 @@
 const mongo = require('mongodb').MongomainmainClient;
 const mainClient = require('socket.io').listen(4000).sockets;
+//For login auth
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//Connect to database
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database);
+});
+
+mongoose.connection.on('error', () => {
+    console.log('Database error ' + err);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+//Port number
+const port = 3000;
+
+//CORS middleware
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+//Getting rid of "cannot get/" & index route
+
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+})
+
+//start server
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
 
 // Connect to mongo
 mongo.connect('mongodb://127.0.0.1/gahrpals', function(err, client){
