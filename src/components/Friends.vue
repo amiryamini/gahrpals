682f4af0ab7326eb004d1be268112a77e218bdb1
amiryamini <amir.yamini.416@my.csun.edu>
2018-05-07 19:33:21 -0700

    Trying to update my branch with whatever samir did

---
+++ src/components/Friends.vue
@@ -0,0 +1,112 @@
+<template>
+  <!--Friends section of the page-->
+  <section class="">
+    <div class="card mx-4 my-4 px-4 py-4">
+      <div class="card-body ">
+        <h5 class="card-title">Friends</h5>
+
+        <!--have some friends! friends listed below consist of their
+            icon
+            name
+            -->
+
+
+        <!--ADD FRIEND -->
+        <div class="friend col-md-2 col-sm-3 col-xs-4 mx-2">
+          <div class="__icon text-center">
+            <!--TODO: friend's icon image here-->
+            <h1>+</h1>
+          </div>
+
+          <div class="__name text-center">
+            <!--TODO: add friend functionality-->
+            <a href="#">
+              <h5>ADD FRIEND</h5>
+            </a>
+          </div>
+
+          <div class="__deleteButton text-center invisible">
+            <!--TODO: delete friend functionatlity-->
+            <button class="btn btn-outline-info text-center" type="button">delete</button>
+          </div>
+          <!--end div friend-->
+        </div>
+
+
+
+        <!--A single friend... my only friend... -->
+        <div class="friend col-md-2 col-sm-3 col-xs-4 mx-2">
+          <div class="__icon text-center">
+            <!--TODO: friend's icon image here-->
+            <img src="../assets/logo.png" alt="" width="50">
+          </div>
+
+          <div class="__name text-center">
+            <!--TODO: friend's name here, and link to their profile-->
+            <a href="#">
+              <h5>qwerty asdfafr</h5>
+            </a>
+          </div>
+
+          <div class="__deleteButton text-center">
+            <!--TODO: delete friend functionatlity-->
+            <button class="btn btn-outline-info text-center" type="button">delete</button>
+          </div>
+          <!--end div friend-->
+        </div>
+
+
+
+
+        <!--A single friend... my only friend... -->
+        <div class="friend col-md-2 col-sm-3 col-xs-4 mx-2">
+          <div class="__icon text-center">
+            <!--TODO: friend's icon image here-->
+            <img src="../assets/logo.png" alt="" width="50">
+          </div>
+
+          <div class="__name text-center">
+            <!--TODO: friend's name here, and link to their profile-->
+            <a href="#">
+              <h5>qwerty asdfafr</h5>
+            </a>
+          </div>
+
+          <div class="__deleteButton text-center">
+            <!--TODO: delete friend functionatlity-->
+            <button class="btn btn-outline-info text-center" type="button">delete</button>
+          </div>
+          <!--end div friend-->
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style scoped>
+  .card {
+    border-radius: 10px;
+    box-shadow: 2px 2px 3px #888888;
+  }
+  .friend {
+    background-color: #fff;
+    display: inline-block;
+  }
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <!--actual login template-->
+  <section>
+    <!--container will hold the login form stuff-->
+    <div class="container mt-4">
+
+      <form class="mx-auto col-xs-12 col-sm-10 col-md-5">
+        <div class="login__icon">
+          <h1 class="text-center">Garhpals</h1>
+        </div>
+
+        <div class="form-group mt-4">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="form-group mt-4">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+
+        <!--TODO: link to My Profile page; log in the user-->
+        <button type="submit" class="btn btn-primary mt-4">Log in</button>
+
+        <!--TODO: link to Register page-->
+        <h5 class="text-center mt-4">New member? <a href="#">Register</a></h5>
+
+      </form>
+      <!--end div container-->
+    </div>
+  </section>
+</template>
+
+
+
+<!--scripts for stuff-->
+<script>
+
+</script>
+
+
+
+
+<!-- Added "scoped" attribute to limit CSS to this component only --><!-- Added "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  section .container {
+    background-color: #fff;
+  }
+
+  .login__icon {
+    margin-top: 50px;
+  }
+
+</style>
